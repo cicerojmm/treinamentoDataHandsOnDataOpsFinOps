c9f2fbb684bc900bb4e0b47efda8752c0333ca9c
cicerojmm@gmail.com <cicerojmm@gmail.com>
2025-12-09 14:41:51 -0300

    adicionar applications para finops

--- terraform/infra/main.tf
+++ terraform/infra/main.tf
@@ -581,15 +581,15 @@ module "emr_serverless" {
 
   release_label = "emr-7.0.0"
 
-  driver_cpu    = "4 vCPU"
-  driver_memory = "16 GB"
+  driver_cpu    = "2 vCPU"
+  driver_memory = "8 GB"
 
   executor_count  = 2
-  executor_cpu    = "4 vCPU"
-  executor_memory = "16 GB"
+  executor_cpu    = "2 vCPU"
+  executor_memory = "8 GB"
 
   max_cpu    = "20 vCPU"
-  max_memory = "40 GB"
+  max_memory = "60 GB"
 
   idle_timeout_minutes = 5
 }
